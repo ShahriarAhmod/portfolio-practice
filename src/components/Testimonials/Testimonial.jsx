f33222a1b61e78813f7778952dc9285a7da4e912
ShahriarAhmod <ashahriyar71@gmail.com>
2022-04-16 17:15:28 +0600

    first

---
+++ src/components/Testimonials/Testimonial.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import "./Testimonial.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
+import { Pagination } from "swiper";
+import "swiper/css/pagination";
+import profilePic1 from "../../img/profile1.jpg";
+import profilePic2 from "../../img/profile2.jpg";
+import profilePic3 from "../../img/profile3.jpg";
+import profilePic4 from "../../img/profile4.jpg";
+
+const Testimonial = () => {
+  const clients = [
+    {
+      img: profilePic1,
+      review:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex officiis molestiae quod tempora laudantium, cumque error a nisi placeat quae exercitationem, maiores reiciendis.",
+    },
+    {
+      img: profilePic2,
+      review:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex officiis molestiae quod tempora laudantium, cumque error a nisi placeat quae exercitationem, maiores reiciendis.",
+    },
+    {
+      img: profilePic3,
+      review:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex officiis molestiae quod tempora laudantium, cumque error a nisi placeat quae exercitationem, maiores reiciendis.",
+    },
+    {
+      img: profilePic4,
+      review:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex officiis molestiae quod tempora laudantium, cumque error a nisi placeat quae exercitationem, maiores reiciendis.",
+    },
+  ];
+
+  return (
+    <div className="t-wrapper" id="testimonial">
+      <div className="t-heading">
+        <span>Clients always get </span>
+        <span>Exceptional Work </span>
+        <span>from me...</span>
+      <div className="blur t-blur1" style={{ background: "var(--purple)" }}></div>
+      <div className="blur t-blur2" style={{ background: "skyblue" }}></div>
+
+      </div>
+      <Swiper
+        // install Swiper modules
+        modules={[Pagination]}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+      >
+        {clients.map((client, index) => {
+          return (
+            <SwiperSlide key={index}>
+              <div className="testimonial">
+                <img src={client.img} alt="" />
+                <span>{client.review}</span>
+                <div className='fs-4'>
+                  <span><i class="fas fa-star col"></i></span>
+                  <span><i class="fas fa-star col"></i></span>
+                  <span><i class="fas fa-star col"></i></span>
+                  <span><i class="fas fa-star col"></i></span>
+                  <span><i class="fas fa-star col"></i></span>
+                </div>
+              </div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </div>
+  );
+};
+
+export default Testimonial;
